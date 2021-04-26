8b52b058ca574e98baa06e340c2336ce4b342436
kpedrok <pedrokvb@gmail.com>
2021-04-26 01:26:25 -0300

    Update chart

--- pages/test.js
+++ pages/test.js
@@ -1,18 +1,18 @@
 import Head from "next/head";
-import React, { useEffect } from "react";
+import React from "react";
 import styles from "../styles/Home.module.css";
 
 function Home() {
-  function removeElementsByClass(className) {
-    var elements = document.getElementsByClassName(className);
-    if (elements?.length > 0) {
-      elements[0].parentNode.removeChild(elements[0]);
-    }
-  }
+  // function removeElementsByClass(className) {
+  //   var elements = document.getElementsByClassName(className);
+  //   if (elements?.length > 0) {
+  //     elements[0].parentNode.removeChild(elements[0]);
+  //   }
+  // }
 
-  useEffect(() => {
-    removeElementsByClass("flourish-credit");
-  });
+  // useEffect(() => {
+  //   removeElementsByClass("flourish-credit");
+  // });
 
   return (
     <div>
@@ -33,28 +33,13 @@ function Home() {
             frameBorder="0"
             scrolling="no"
             className={styles.flourishContainer}
-            // style="width: 100%; height: 600px"
             sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation"
           ></iframe>
-          {/* <div style="width: 100%!; margin-top: 4px !important; text-align: right !important"> */}
-          <a
-            className="flourish-credit"
-            href="https://public.flourish.studio/visualisation/5964566/?utm_source=embed&utm_campaign=visualisation/5964566"
-            target="_top"
-            // style="text-decoration: none !important"
-          >
-            <img
-              alt="Made with Flourish"
-              src="https://public.flourish.studio/resources/made_with_flourish.svg"
-              //       style="
-              //   width: 105px !important;
-              //   height: 16px !important;
-              //   border: none !important;
-              //   margin: 0 !important;
-              // "
-            />
-          </a>
-          {/* </div> */}
+          <div
+            className={
+              "width: 90vw !important; margin-top: 4px !important; text-align: right !important"
+            }
+          ></div>
         </div>
       </main>
 
@@ -64,7 +49,7 @@ function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
+          Powered by
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
