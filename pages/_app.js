681faa62a58c5c19625972f5cdee1f5ee071707a
kpedrok <pedrokvb@gmail.com>
2021-04-26 02:28:54 -0300

    Update gtag

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,19 @@
+import React, { useEffect } from "react";
+import ReactGA from "react-ga";
 import "../styles/globals.css";
 
+ReactGA.initialize(process.env.GOOGLE_ANALYTICS_TRACKING_ID);
+const trackPage = (page) => {
+  ReactGA.set({ page });
+  ReactGA.pageview(page);
+};
+
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const page = location.pathname;
+    trackPage(page);
+  });
+
   return <Component {...pageProps} />;
 }
 
