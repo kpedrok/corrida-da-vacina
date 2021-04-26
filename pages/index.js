ad9a6dbaf891b370756559a6b7f7bb0462c84be2
kpedrok <pedrokvb@gmail.com>
2021-04-26 01:39:11 -0300

    Update home page

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,53 @@
 import Head from "next/head";
+import React from "react";
 import styles from "../styles/Home.module.css";
 
 function Home() {
+  // function removeElementsByClass(className) {
+  //   var elements = document.getElementsByClassName(className);
+  //   if (elements?.length > 0) {
+  //     elements[0].parentNode.removeChild(elements[0]);
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   removeElementsByClass("flourish-credit");
+  // });
+
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <div>
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Corrida da <a href="./test">Vacina</a>
-        </h1>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Corrida da <span className={styles.span}>Vacina!</span>
+          </h1>
 
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          <div className={styles.background}>Teste TEEETEE</div>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <div className={styles.flourishTest}>
+            <iframe
+              src="https://flo.uri.sh/visualisation/5964566/embed"
+              title="Interactive or visual content"
+              frameBorder="0"
+              scrolling="no"
+              className={styles.flourishContainer}
+              sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation"
+            ></iframe>
+            <div
+              className={
+                "width: 90vw !important; margin-top: 4px !important; text-align: right !important"
+              }
+            ></div>
+          </div>
+        </main>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        {/* <footer className={styles.footer}></footer> */}
+      </div>
     </div>
   );
 }
