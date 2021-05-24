5de1575515b9962cd2674e3a52c0f14d78560951
Pedro Boas <pedrokvb@gmail.com>
2021-05-24 10:29:58 -0300

    Add google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,27 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import { GA_TRACKING_ID } from "../utils/gtag";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+          `,
+          }}
+        />
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
