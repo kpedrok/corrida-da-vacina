64c4d369f22bb95c099469037b09d24f32ff8b5a
Pedro Boas <pedrokvb@gmail.com>
2021-05-24 10:06:49 -0300

    Add google analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
+import ReactGa from "react-ga";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    ReactGa.initialize("G-XBJFETXKRK");
+  }, []);
+
+  ReactGa.pageview("/");
   return <Component {...pageProps} />;
 }
 
